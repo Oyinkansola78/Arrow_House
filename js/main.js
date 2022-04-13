3159657b4eba7d54356223767d42d25bf7f62d8c
Oyinkansola78 <oyinkanayannuga@gmail.com>
2022-04-13 15:02:26 +0100

    'FAQ_page_done'

---
+++ js/main.js
@@ -0,0 +1,6 @@
+// const collapseElementList = [].slice.call(document.querySelectorAll('.collapse'))
+// const collapseList = collapseElementList.map((collapseEl) => {
+//   return new mdb.Collapse(collapseEl, {
+//     toggle: false,
+//   });
+// });
